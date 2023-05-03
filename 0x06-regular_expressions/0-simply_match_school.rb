09b173a7a3eb7a871d99ad2fe94650e20871c5b2
Elias <eliaseshetu28@gmail.com>
2023-05-03 17:57:08 +0300

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
 def regexp
-  if ARGV[0].match(/School/)
-      print "School"
+  if ARGV[0] == nil
+         return
+  end
+  list = ARGV[0].split()
+  for check in list do
+    if check.match(/School/)
+        print "School"
+    end
   end
 end
 regexp
