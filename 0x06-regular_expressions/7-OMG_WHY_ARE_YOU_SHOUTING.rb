8d6bc176b26efda33b77662aa5a427277eb9fbf0
Elias <eliaseshetu28@gmail.com>
2023-05-03 23:07:19 +0300

    7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -4,13 +4,9 @@ def regexp
          return
   end
   list = ARGV[0].split()
-  puts list
   united = list.join(',')
-  puts united
   disunited = united.split('')
-  puts "x"
-  puts disunited
-  
+
   for cap in disunited 
     if cap.match(/[A-Z]/)
      print "#{cap}"
