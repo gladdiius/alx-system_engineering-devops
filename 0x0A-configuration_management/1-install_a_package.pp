0ca5da76118e43eae0390a01f5c046a7cc662e24
elias_eshetu <eliaseshetu28@gmail.com>
2023-11-24 21:28:34 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
 # install_flask.pp
 
-package { 'python3-pip':
-  ensure => present,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin:/usr/local/bin',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
