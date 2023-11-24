08af1ef59cdf9be46f2ef4e2cb677746d5340567
elias_eshetu <eliaseshetu28@gmail.com>
2023-11-24 22:05:58 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 #!/usr/bin/pup
-# install Flask version 2.1.0 using the pip3 provider
-package {'flask':
+# install_flask.pp
+# Puppet manifest to install Flask version 2.1.0 using the pip3 provider
+
+# install_flask.pp
+# Puppet manifest to install Flask version 2.1.0 using the pip3 provider
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  notify   => Exec['print_flask_notice'],
 }
+
+exec { 'print_flask_notice':
+  command => '/bin/echo "Flask installation completed"',
+}
+
+
