ebd7bda0de466739f3d4d8314d4ed85f09366879
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-07 11:21:24 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,23 @@
-file { '/home/ubuntu/.ssh/config': # Replace 'your_username' with your actual username
-  ensure => file,
-  content => "\
-Host *
-    IdentityFile ~/.ssh/school
-",
-  mode => '0600',
-  owner => 'your_username', # Replace 'your_username' with your actual username,
+# Ensure the SSH client configuration file exists
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+  mode   => '0644',
+  owner  => 'root',
+  group  => 'root',
 }
 
+# Set up SSH client configuration to use the private key ~/.ssh/school
+file_line { 'ssh_private_key':
+  path    => '/etc/ssh/ssh_config',
+  line    => '   IdentityFile ~/.ssh/school',
+  match   => '^#?IdentityFile ~/.ssh/school',
+  require => File['/etc/ssh/ssh_config'],
+}
+
+# Set up SSH client configuration to refuse password authentication
+file_line { 'ssh_password_authentication':
+  path    => '/etc/ssh/ssh_config',
+  line    => '   PasswordAuthentication no',
+  match   => '^#?PasswordAuthentication',
+  require => File['/etc/ssh/ssh_config'],
+}
