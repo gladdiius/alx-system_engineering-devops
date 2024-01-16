bbd2fe70ecccd35fcbea11c4f969a26d928bf219
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-16 19:57:59 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,25 +3,26 @@
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit.
+if __name__ == '__main__':
+    def number_of_subscribers(subreddit):
+        """Return the number of subscribers for a given subreddit.
 
-    Args:
-    subreddit (str): The name of the subreddit.
+        Args:
+        subreddit (str): The name of the subreddit.
 
-    Returns:
-    int: The number of subscribers for the subreddit.
-    0: If the subreddit is invalid or any other error occurs.
-    """
+        Returns:
+        int: The number of subscribers for the subreddit.
+        0: If the subreddit is invalid or any other error occurs.
+        """
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
+        url = f'https://www.reddit.com/r/{subreddit}/about.json'
+        headers = {'User-Agent': 'MyRedditBot/1.0'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
