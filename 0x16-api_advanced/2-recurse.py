eb94591325306ed8a762b71bbbce673d6f943e8a
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-16 19:38:17 +0300

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
+""" recurseion"""
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Recursively retrieve the titles of hot posts in a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+        hot_list (list): A list to store the titles of
+            hot posts (optional, default is an empty list).
+
+        after (str): A token used for pagination to retrieve the next set
+            of results (optional, default is None).
+
+        Returns:
+            list: A list containing the titles of hot posts in the subreddit.
+            None: If the subreddit is invalid or any other error occurs.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+
     params = {'limit': 100, 'after': after}
+
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code != 200:
