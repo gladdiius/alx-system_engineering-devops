50b77e2f324f02c2af3c04851ec2b6c9cc8147b8
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-16 19:47:00 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" returns number of subscripion."""
+""" number fo subscriber"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Get the number of subscribers for a given subreddit
+    """Return the number of subscribers for a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit.
+    subreddit (str): The name of the subreddit.
 
     Returns:
-        int: The number of subscribers of the subreddit.
-        Returns 0 if the subreddit is invalid or if any 
-        other error occurs.
+    int: The number of subscribers for the subreddit.
+    0: If the subreddit is invalid or any other error occurs.
     """
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyRedditBot/1.0'}
-    response = requests.get(url, headers=headers)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
