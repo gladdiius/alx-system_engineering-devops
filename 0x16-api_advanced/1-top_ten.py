5b68888f702b753d62ce246e7a07cab8b5f8065f
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-16 19:25:24 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,31 @@
+#!/usr/bin/python3
+"""returns the top 10 title"""
 import requests
 
+
 def top_ten(subreddit):
-    # Reddit API endpoint for getting the hot posts in a subreddit
+    """Print the titles of the top 10 hot posts in a given subreddit.
+
+    Args:
+    subreddit (str): The name of the subreddit.
+
+    Returns:
+    None
+    """
+
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    
+
     # Set a custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': 'MyRedditBot/1.0'}
-    
+
     # Make the GET request to the Reddit API
     response = requests.get(url, headers=headers)
-    
+
     # Check if the request was successful (status code 200)
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
-        
+
         # Extract and print the titles of the first 10 hot posts
         for post in data['data']['children']:
             title = post['data']['title']
@@ -22,4 +33,3 @@ def top_ten(subreddit):
     else:
         # If the subreddit is invalid or any other error occurs, print None
         print(None)
-
