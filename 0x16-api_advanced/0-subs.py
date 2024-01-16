1d8f7e48b7a3c04ec472c599f73af048f487b983
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-16 09:44:48 +0300

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    # Reddit API endpoint for getting subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    
+    # Set a custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    
+    # Make the GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+    
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+        
+        # Extract the number of subscribers from the response
+        subscribers = data['data']['subscribers']
+        
+        return subscribers
+    else:
+        # If the subreddit is invalid or any other error occurs, return 0
+        return 0
+
