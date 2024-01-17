276d6ae74ec792469ccda3412000f6d58c54dc69
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-17 21:15:06 +0300

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,24 @@
 # Puppet Manifest: 0-strace_is_your_friend.pp
-# Description: Fix Apache 500 error using strace and automate with Puppet
+# Description: Attempt to fix Apache 500 error using strace and automate with Puppet
+
+# Define an exec resource to run strace on Apache
+exec { 'strace-apache':
+  command     => '/usr/bin/strace -f -s 1024 -o /tmp/strace_output.txt -p $(pidof apache2)',
+  path        => ['/usr/bin'],
+  refreshonly => true,
+  notify      => Exec['fix-apache-issue'],
+}
 
 # Define an exec resource to fix the identified issue
 exec { 'fix-apache-issue':
-  command     => '/path/to/fix_script.sh',  # Replace with the actual path to your fix script
+  command     => '/path/to/fix_script.sh',
+  path        => ['/bin', '/usr/bin'],
   refreshonly => true,
-  subscribe   => Service['apache2'],  # Replace 'apache2' with the actual service name
+  subscribe   => Service['apache2'],
 }
 
 # Notify Apache service restart when the fix script is applied
-service { 'apache2':  # Replace 'apache2' with the actual service name
+service { 'apache2':
   ensure  => 'running',
   require => Exec['fix-apache-issue'],
 }
